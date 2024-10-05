47f76f6c761c13bbdbef5015dba73c6d8fa94e6c
sahil rane <sahilrane249@gmail.com>
2024-10-05 15:15:04 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Make sure this is correct
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',
-      },
-    },
-  },
-  
-});
+})
